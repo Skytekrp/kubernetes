9b2a3747056e8759011ab945861bc282c2d5fbb3
Skytekrp <128128977+Skytekrp@users.noreply.github.com>
2023-07-03 22:49:46 +0200

    update 2

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform   {
+    backend "azurerm" {
+      resource_group_name  = "kopicloud-tstate-rg"
+      storage_account_name = "kopicloudtfstate34756"
+      container_name       = "tfstate1"
+      key                  = "terraform.tfstate"
+     }
+
+}
--- main.tf
+++ main.tf
@@ -6,12 +6,7 @@ terraform {
       version = "~> 3.0.0"
     }
   }
-    backend "azurerm" {
-      resource_group_name  = "kopicloud-tstate-rg"
-      storage_account_name = "kopicloudtfstate34756"
-      container_name       = "tfstate1"
-      key                  = "terraform.tfstate"
-  }
+    
 }
 provider "azurerm" {
   features {}
